b6a74ab6bd8f26a2d7b7e6b57a721c1a7884213a
2goodAP <36451354+2goodAP@users.noreply.github.com>
2018-07-09 21:36:32 +0545

    Some more cleanup

--- win.cpp
+++ win.cpp
@@ -1,7 +1,7 @@
-#include <SFML/Window.hpp>
+/*#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <iostream>
-/*int main()
+int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 1000), "QUIZMA");
     sf::CircleShape circleRed(50);
